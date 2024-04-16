c521aef1cb309ae8005a7332974e4fb6fbc8df34
Sepehr-Glpz <sepehrgolpazir@gmail.com>
2024-04-16 15:30:01 +0330

    feat(config): Added Configuration DTO classes
    Added config classes for broker, exchanges, queues and bindings

---
+++ RabbitClient/Configuration/BrokerConfig.cs
@@ -0,0 +1,17 @@
+﻿namespace SGSX.RabbitClient.Configuration;
+
+internal class BrokerConfig
+{
+    public required string Username { get; set; }
+    public required string Password { get; set; }
+    public required string VirtualHost { get; set; }
+    public string? DefaultHost { get; set; }
+    public ushort? DefaultPort { get; set; } = 5672;
+    public required List<BrokerEndpoint> Endpoints { get; set; }
+}
+
+internal class BrokerEndpoint
+{
+    public string? Host { get; set; }
+    public ushort? Port { get; set; }
+}
