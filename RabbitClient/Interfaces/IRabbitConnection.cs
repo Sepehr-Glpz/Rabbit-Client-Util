f1ef06725e9c5461ba1b2b57e5a43714b1cf2964
Sepehr-Glpz <sepehrgolpazir@gmail.com>
2024-04-17 12:26:46 +0330

    feat(connection): Added Connection Handling
    Added Connection Handler and Channel Hnadler Classes and interface

---
+++ RabbitClient/Interfaces/IRabbitConnection.cs
@@ -0,0 +1,15 @@
+﻿
+using RabbitMQ.Client;
+using System.Threading;
+
+namespace SGSX.RabbitClient.Interfaces;
+public interface IRabbitConnection : IDisposable
+{
+    Task ConnectAsync(CancellationToken ct);
+
+    Task CloseAsync(CancellationToken ct);
+
+    IConnection InnerConnection { get; }
+
+    bool IsDisposed { get; }
+}
